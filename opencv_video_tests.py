ba6ca49026faf5f2e939d09bf49bceb2303e3524
wesmith <smiwarsky@gmail.com>
2022-01-18 04:01:50 -0700

    stripped out extraneous processing, to measure fps and attempt to measure latency

--- opencv_video_tests.py
+++ opencv_video_tests.py
@@ -1,22 +1,35 @@
+#!/usr/bin/env python3
+
+# WESmith 01/18/22 run webcam and picam from opencv on the rpi 4.
+
 
 import cv2
+from time import time
+
 print(cv2.__version__)
+
 scale = 2
 dispW = 320 * scale
 dispH = 240 * scale
 flip = 0
 
-picam = True # WS mod
+fps   = 15.0 #  a rough initial value
+t_old = time()
+# alpha: smoothing factor for frame/sec estimatee
+# float 0 to 1; the larger alpha, the more smoothing
+alpha = 0.95
+wid_hei = (dispW, dispH)
 
-if picam: 
-    # gstreamer command line for pi camera
-    # WS note: picam is hooked to '0' connector, the other is '1': apparently '0' runs by default
-    #          if a second picam is used and hooked to '1' slot, then need to add a new
-    #          channel variable to the command below: see comments in McWhorter vid (which one?)
-    #          to see appropriate variable to add
-    camSet='nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-    cam = cv2.VideoCapture(camSet)
+
+picam = True
+
+if picam:
+    # note: picam is hooked to the '0' connector, the webcam is '1':
+    cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH,  dispW)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, dispH)
     camNam = 'piCam'
+
 else:
     # the following is for the webcam: use '1' if picam used in slot '0'
     cam = cv2.VideoCapture(1)
@@ -24,32 +37,40 @@ else:
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, dispH)
     camNam = 'webCam'
 
-actual_dispW = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-actual_dispH = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print('')
-print('ACTUAL DISPLAY WIDTH, HEIGHT:', actual_dispW, actual_dispH)
+actual_dispW = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))  # this doesn't reflect the 'set' command
+actual_dispH = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)) # ditto
+
+print('\nUSER-DEFINED DISPLAY WIDTH, HEIGHT: {} {}\n'.\
+      format(dispW, dispH))
+
+def add_info(frame, fps):
+    font   = cv2.FONT_HERSHEY_SIMPLEX
+    f_size = 0.40 * scale
+    separation = int(5 * scale)
+    row0   = 30
+    col0   = 10
+    delta  = int(0 * scale)
+    thick  = 2 if scale >= 1 else 1
+    cv2.putText(frame, fps, (col0, row0 - 3 * delta), font, f_size,
+                (120, 255, 120), thick) # light green
+
 
 while True:
+
+    dt    = time() - t_old
+    t_old = time()
+    fps   = alpha * fps + (1 - alpha) / dt
+
     ret, frame = cam.read()
 
-    # impt to create a copy, or changes to frame will show up in ROI
-    roi = frame[50:250, 200:400].copy()  
-    roiGray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    roiGray = cv2.cvtColor(roiGray, cv2.COLOR_GRAY2BGR) # get 3 channels back
+    fpstxt = '{}: {:4.1f} FPS for {} x {}'.format(camNam, fps, *wid_hei)
+    add_info(frame, fpstxt)
 
-    frame[50:250, 200:400] = roiGray
-
-    cv2.imshow('ROI', roi)
-    cv2.imshow('GRAY', roiGray)
     cv2.imshow(camNam, frame)
-
-    cv2.moveWindow('ROI',  dispW + 80, 0)
-    cv2.moveWindow('GRAY', dispW + 80, 260)
-    cv2.moveWindow(camNam, 0, 0)
-
 
     if cv2.waitKey(1) == ord('q'):
         break
+
 cam.release()
 cv2.destroyAllWindows()
 
